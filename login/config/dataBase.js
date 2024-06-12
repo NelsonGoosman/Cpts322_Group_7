dad95cf29c5153cb755f0e403b24869cdaf3bdf3
Ivanq50 <ivanq2802@gmail.com>
2024-06-11 21:51:26 -0700

    Adding backend for signup/signin to master

--- login/config/dataBase.js
+++ login/config/dataBase.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI, {})
+mongoose.connect(process.env.MONGODB_URI, {}) //Connects to mongoDB
 .then(() => {
         console.log('DB Connected');
 }).catch((err) => console.log(err))
