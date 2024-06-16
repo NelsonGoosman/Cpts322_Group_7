4946fd3405154151c85d2db964c7ba1cdc87ae99
NelsonGoosman <nelsongoosman@gmail.com>
2024-06-15 18:06:27 -0700

    Fixed some bugs in frontend, added routes from frontend to backend

--- src/app/create-account/create-account.component.ts
+++ src/app/create-account/create-account.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Injectable } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { homedir } from 'os';
 import { HomeComponent } from '../home/home.component';
 import { RouterLink, RouterModule, Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { response } from 'express';
+
 @Component({
   selector: 'app-create-account',
   standalone: true,
@@ -10,14 +13,32 @@ import { RouterLink, RouterModule, Router } from '@angular/router';
   templateUrl: './create-account.component.html',
   styleUrl: './create-account.component.css'
 })
+
+@Injectable({providedIn: 'root'})
 export class CreateAccountComponent {
   newEmail = "";
   newPassword = "";
-  constructor(private router: Router){
+  name = "TestName";
+  constructor(private router: Router, private http: HttpClient){
 
   }
+  dataSent(){
+    alert();
+  }
+  error(){}
   create(){
-    
-      this.router.navigate(['/home'])
+    const userdata = {
+      name: this.name,
+      email: this.newEmail,
+      password: this.newPassword
+    };
+
+  
+
+    this.http.post('/Server/user/signup', userdata).subscribe({
+      next: (v: any) => alert(v),
+      error: (e: any) => alert(e)
+  });
+    //this.router.navigate(['/home'])
   }
 }
