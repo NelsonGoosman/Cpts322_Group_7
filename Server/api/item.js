dc16eac6a673df729f5c3ec63cba355973178e64
NelsonGoosman <nelsongoosman@gmail.com>
2024-06-28 21:04:49 -0700

    Data entry page finished

--- Server/api/item.js
+++ Server/api/item.js
@@ -3,20 +3,32 @@ const router = express.Router();
 
 const Item = require('../models/item');
 
-router.post('/enterItems', (req,res) =>{
-    const data = req.body;
-    console.log(data);
-    res.status(200).send({ message: 'Data received:' });
-    for (const item of items){
-        const newItem = new Item({
-            name: data.name,
-            description: data.description,
-            expiration: data.expiration,
-            cost: data.price,
-            donatedBy: data.donatedBy
-        })
-        newItem.save();
+router.post('/enterItems', async (req, res) => {
+    const items = req.body; // Assuming req.body.items is an array of item objects
+
+    // if (!items || !Array.isArray(items)) {
+    //     return res.status(400).send({ message: 'Invalid data format' });
+    // }
+    
+    console.log(req.body);
+    try {
+        for (const data of items) {
+            const newItem = new Item({
+                name: data.name,
+                description: data.description,
+                expiration: data.expiration,
+                cost: data.price,
+                donatedBy: data.donatedBy
+            });
+
+            await newItem.save();
+        }
+
+        res.status(200).send({ message: 'Data received and saved successfully' });
+    } catch (error) {
+        console.error('Error saving items:', error);
+        res.status(500).send({ message: 'Error saving items' });
     }
-})
+});
 
 module.exports = router;
